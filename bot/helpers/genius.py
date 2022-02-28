184e6617681c59597de4ee68378a9663f177aaef
Steven Ondieki omwange <ondiekisteven@users.noreply.github.com>
2022-02-28 09:25:37 +0300

    Update genius.py
    add user-agent header

--- bot/helpers/genius.py
+++ bot/helpers/genius.py
@@ -20,6 +20,7 @@ class Genius:
     def get_header(self):
         header = {
             'Authorization': f'Bearer {self.token}',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
         }
         return header
 
